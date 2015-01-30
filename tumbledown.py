980187c25b60b795bbd71e1c0842f34b1d116f02
Szymon Pyżalski <szymon.pyzalski@allegro.pl>
2015-01-30 17:07:03 +0100

    Getting all URLs

--- tumbledown.py
+++ tumbledown.py
@@ -3,22 +3,50 @@
 import aiohttp
 import asyncio
 from pprint import pprint
+import re
 
 API_KEY = 'IMweFRGfcZYzDgCgFxU5xxjsDpYfTi3HunVvzX0cUhz8mW0eXQ'
 URL_BASE = 'http://api.tumblr.com/v2/blog/quandmonchat.tumblr.com/'
+PAGE_LIMIT = 20
+URL_REGEX = re.compile(r'src="([^"]+)"')
+
+@asyncio.coroutine
+def request(api_method, additional_params = None):
+    additional_params = additional_params or {}
+    params = {
+        'api_key': API_KEY
+    }
+    params.update(additional_params)
+    return aiohttp.request('GET', URL_BASE + api_method, params=params)
+
+@asyncio.coroutine
+def get_result_page(offset):
+    print('Getting page with offset {}'.format(offset))
+    response = yield from request('posts', {
+        'offset': offset,
+        'limit': PAGE_LIMIT,
+    })
+    data = yield from response.json()
+    for post in data['response']['posts']:
+        match = URL_REGEX.search(post.get('body', ''))
+        if match:
+            url = match.group(1)
+            print(url)
+
 
 
 @asyncio.coroutine
 def get_blog_info():
-    response = yield from aiohttp.request(
-        'GET',
-        URL_BASE + 'info',
-        params = {
-            'api_key': API_KEY
-        }
-    )
+    response = yield from request('info')
     data = yield from response.json()
-    pprint(data)
+    todo = data['response']['blog']['posts']
+    offset = 0
+    tasks = []
+    while offset < todo:
+        tasks.append(get_result_page(offset))
+        offset += PAGE_LIMIT
+    yield from asyncio.wait(tasks)
+
 
 
 if __name__ == '__main__':
